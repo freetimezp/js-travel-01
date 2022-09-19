8f7d425afeb76971b0e48f9447fec45aaf93dfaa
freetimezp <kraftukrnet@ukr.net>
2022-09-19 17:45:29 +0300

    14. js-travel-01. Change theme mode.

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,8 +117,38 @@ window.addEventListener('scroll', scrollActive);
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
 
+//selected topic if user selected
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
+//get current theme by validating dart theme class
+const getCurrentTheme = () => {
+    document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+}
+const getCurrentIcon = () => {
+    themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+}
+
+//validate if user previously choose a topic
+if(selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+//activate/deactivate theme with button
+themeButton.addEventListener('click', () => {
+    //add or remove dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    //save theme and icon that user choose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
 
 
 
